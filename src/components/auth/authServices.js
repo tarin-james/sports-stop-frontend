bb1e56390a9be63c926eb7d9379a0b7baeff0834
tarin-james <tarinwardwell@gmail.com>
2025-04-07 23:16:37 -0600

    Revert

--- src/components/auth/authServices.js
+++ src/components/auth/authServices.js
@@ -1,21 +1,9 @@
 import axios from "axios";
 
 export function fetchUser() {
-  const headers = {};
-  const sessionCookie = getCookie("connect.sid");
-  if (sessionCookie) headers["Cookie"] = `connect.sid=${sessionCookie}`;
 
   return axios.get("https://sports-stop.onrender.com/auth", {
     withCredentials: true,
-    headers,
   });
 }
 
-const getCookie = (name) => {
-  const cookies = document.cookie;
-  const cookieArray = cookies.split(";");
-  const cookie = cookieArray.find((cookie) =>
-    cookie.trim().startsWith(name + "=")
-  );
-  return cookie ? cookie.split("=")[1] : null;
-};

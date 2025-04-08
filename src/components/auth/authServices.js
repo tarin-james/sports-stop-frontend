e33d511675f62409d8e372d8d5d538a165cb89ff
tarin-james <tarinwardwell@gmail.com>
2025-04-07 23:11:55 -0600

    Add cookie to axios auth call

--- src/components/auth/authServices.js
+++ src/components/auth/authServices.js
@@ -1,7 +1,21 @@
 import axios from "axios";
 
 export function fetchUser() {
+  const headers = {};
+  const sessionCookie = getCookie("connect.sid");
+  if (sessionCookie) headers["Cookie"] = `connect.sid=${sessionCookie}`;
+
   return axios.get("https://sports-stop.onrender.com/auth", {
     withCredentials: true,
+    headers,
   });
 }
+
+const getCookie = (name) => {
+  const cookies = document.cookie;
+  const cookieArray = cookies.split(";");
+  const cookie = cookieArray.find((cookie) =>
+    cookie.trim().startsWith(name + "=")
+  );
+  return cookie ? cookie.split("=")[1] : null;
+};

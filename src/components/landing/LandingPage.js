75eda0828e97e85352eafcd81d9cfe5a9c911f7e
tarin-james <tarinwardwell@gmail.com>
2025-04-09 23:20:58 -0600

    add sign in

--- src/components/landing/LandingPage.js
+++ src/components/landing/LandingPage.js
@@ -1,9 +1,9 @@
 import { Link } from "react-router-dom";
 import React from "react";
-
-
+import { useUser } from "../auth/AuthProvider";
 
 export function LandingPage() {
+  const { isLoggedIn } = useUser();
   return (
     <div className="text-center p-10">
       <h2 className="text-2xl font-bold">Welcome to Sports Stop</h2>
@@ -11,9 +11,21 @@ export function LandingPage() {
         Buy and sell sporting goods in a dedicated auction platform.
       </p>
       <div className="mt-6">
-        <button className="bg-orange-300 text-black px-4 py-2 rounded mx-2">
-          Create an Auction
-        </button>
+        {isLoggedIn ? (
+          <Link
+            to="/create"
+            className="bg-orange-300 text-black px-4 py-2 rounded mx-2"
+          >
+            Create an Auction
+          </Link>
+        ) : (
+          <a
+            className="bg-orange-300 text-black px-4 py-2 rounded"
+            href="https://sports-stop.onrender.com/login"
+          >
+            Sign In
+          </a>
+        )}
         <Link to="/auctions" className="bg-yellow-200 px-4 py-2 rounded">
           View Auctions
         </Link>
